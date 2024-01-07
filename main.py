1032f293a9c0a1c8dddbb9c14bcc59a66950cb57
FabioArdi <arditfab@students.zhaw.ch>
2024-01-07 17:08:56 +0100

    Code refactored

--- main.py
+++ main.py
@@ -28,22 +28,25 @@ hx.set_reading_format("MSB", "MSB")
 hx.set_reference_unit(referenceUnit)
 hx.reset()
 hx.tare()
+poopWeight = 0
+catWeight = 0
+totalWeight = 0
 print("Tare done!")
 # mq = MQ()
 
-def send_cat_weight(weight):
-    url = "https://poop-tracker-48b06530794b.herokuapp.com/cats/"
-    payload = {"weight": float(weight)}
+def sendCatWeight(weight):
+    url = "https://poop-tracker-48b06530794b.herokuapp.com/weights/"
+    payload = {"weight": int(weight),"cat_ID": 4}
     try:
         response = requests.post(url, json=payload)
         if response.status_code == 200:
-            print("Cat weight inserted successfully!")
+            print("Cat Weight inserted successfully!")
         else:
             print(f"Failed to send data. Status code: {response.status_code}")
     except requests.exceptions.RequestException as e:
         print(f"Error: {e}")
 
-def send_poop_weight(weight):
+def sendPoopWeight(weight):
     url = "https://poop-tracker-48b06530794b.herokuapp.com/poops/"
     payload = {"weight": int(weight)}
     try:
@@ -55,69 +58,84 @@ def send_poop_weight(weight):
     except requests.exceptions.RequestException as e:
         print(f"Error: {e}")        
 
-def update_measurement_history(weight):
+def updateMeasurementHistory(weight):
     if len(measurement_history) >= 5:
         measurement_history.pop(0)  # Remove the oldest measurement
     measurement_history.append(weight)  # Add the new measurement
 
-def calculate_average_history():
+def calculateAverageHistory():
     if measurement_history:
         return sum(measurement_history) / len(measurement_history)
     return 0
 
-def measure_air_quality():
-    perc = mq.MQPercentage()
+def measureAirQuality():
+    #perc = mq.MQPercentage()
     sys.stdout.write("\r")
     sys.stdout.write("\033[K")
     sys.stdout.write("LPG: %g ppm, CO: %g ppm, Smoke: %g ppm" % (perc["GAS_LPG"], perc["CO"], perc["SMOKE"]))
     sys.stdout.flush()
     time.sleep(0.1)
 
+def showSensorValues():
+    sys.stdout.write("\r")
+    sys.stdout.write("\033[K")
+    sys.stdout.write(f"Current Value: {val}, Running Average: {runningAverage}")
+    sys.stdout.flush()
+    time.sleep(0.1)
+
+def showValuesWhilePooping():
+    sys.stdout.write("\r")
+    sys.stdout.write("\033[K")
+    sys.stdout.write(f"Weight increase detected! Current Value: {val}, Running Average: {runningAverage} Cat Weight: {catWeight}" )
+    sys.stdout.flush()
+    time.sleep(0.1)
+
 while True:
     try:
         poopInside = False
-        poop_weight = 0
-        cat_weight = 0
-        total_weight = 0
+        poopWeight = 0
+        catWeight = 0
+        totalWeight = 0
         val = hx.get_weight(5)
-        print(val)
         hx.power_down()
         hx.power_up()
         time.sleep(0.1)
 
-        # measure_air_quality()
+        # measureAirQuality()
 
-        update_measurement_history(val)  # Update the measurement history
-        running_average = calculate_average_history()  # Calculate the running average
-        print(f"Running Average: {running_average}")
+        updateMeasurementHistory(val)  # Update the measurement history
+        runningAverage = calculateAverageHistory()  # Calculate the running average
+        showSensorValues()
 
         while val > 100:
-            print("Weight increase detected.")
+            # print("Weight increase detected.")
             val = hx.get_weight(5)
-            print(val)
+            # print(val)
             hx.power_down()
             hx.power_up()
             time.sleep(0.1)
-            total_weight = 0
+            totalWeight = 0
             
             for _ in range(counter):
                 weight = hx.get_weight(5)
-                total_weight += weight
+                totalWeight += weight
                 time.sleep(0.1)
-            cat_weight = total_weight / counter
-            print(f"Cat Weight: {cat_weight}")
+            catWeight = totalWeight / counter
+            # print(f"Cat Weight: {catWeight}")
+            showValuesWhilePooping()
             poopInside = True
 
         if poopInside:
-            send_cat_weight(cat_weight)
-            total_weight = 0
+            sendCatWeight(catWeight)
+            totalWeight = 0
             for _ in range(counter):
                 weight = hx.get_weight(5)
-                total_weight += weight
+                totalWeight += weight
                 time.sleep(0.1)
-            poop_weight = total_weight / counter
-            print(f"Poop Weight: {poop_weight}")
-            send_poop_weight(poop_weight)
+            poopWeight = totalWeight / counter
+            #print(f"Poop Weight: {poopWeight}")
+            time.sleep(2)
+            sendPoopWeight(poopWeight)
             poopInside = False
 
 
